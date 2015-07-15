6641db79a33ca4011f2d0a5889887525e49aea44
Jeffrey Baumes <jeff.baumes@kitware.com>
2015-07-15 11:00:26 -0400

    Removing dead code

--- app/gist.py
+++ app/gist.py
@@ -8,18 +8,3 @@ g = GitHub(os.environ['GITHUB_USERNAME'], os.environ['GITHUB_PASSWORD'])
 
 def run(id):
     return g.gist(id).as_dict()
-
-    # Resolve URLs
-    # if 'vega.json' in gist['files']:
-    #     spec = json.loads(gist['files']['vega.json']['content'])
-    #     if 'data' in spec:
-    #         for d in spec['data']:
-    #             if 'url' in d:
-    #                 if d['url'].startswith('http'):
-    #                     d['values'] = requests.get(d['url']).json()
-    #                     del d['url']
-    #                 else:
-    #                     if d['url'] in gist['files']:
-    #                         d['values'] = json.loads(gist['files'][d['url']]['content'])
-    #                         del d['url']
-    #     gist['files']['vega.json']['content'] = json.dumps(spec)
